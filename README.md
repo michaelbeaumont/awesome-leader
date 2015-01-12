8ba147639da71bbeb8b73f6055ed001de9a30541
michaelbeaumont <mjboamail@gmail.com>
2015-01-12 20:07:52 +0100

    Added timeout and tracking modifier key presses

--- README.md
+++ README.md
@@ -9,13 +9,23 @@ Instructions
 Require the module at the top of `rc.lua`, bind the root leader to a key, and add key combinations:
 
     local leader = require "awesome-leader"
-    awful.key({ modkey }, "\\", leader.root_leader)
+    awful.key({ modkey }, "\\", leader.get_leader())
 
     leader.add_key("ab", function(args)
                        print(table.concat(args.keys) .. "," .. args.count)
                        print("ab expected") end)
 
+Optionally, you can get a link directly to a word:
+
+    awful.key({ modkey }, "a", leader.get_leader("a"))
+
+or change the timeout (default is 1 second):
+
+    leader.set_timeout(2)
+
 The table passed to every function includes the count argument as a number, and all keys pressed so far as an array.
 So after adding the above and typing `<modkey-\>11ab`, the function will print "ab,11".
+A direct link to "a" allows us to type `<modkey-a>11b`, which will print "b,11"
+
 
 Note that adding a key combination "abc" will overwrite the functions set for "a", "ab" and the mappings for any words with the prefix "abc".
--- init.lua
+++ init.lua
@@ -5,6 +5,7 @@ local table = table
 local math = math
 local awful = require "awful"
 local wibox = require "wibox"
+local timer = timer
 local capi =
     {
         client = client,
@@ -16,7 +17,7 @@ local capi =
 local minilog = require "lua-minilog"
 local logger = minilog.logger('off')
 
-local leader = {global_map = {}}
+local leader = {timeout = 1, global_map = {}}
 
 local function access_map(word, new_map)
     if new_map then leader.global_map[word] = new_map end
@@ -26,22 +27,33 @@ end
 
 --create a new grabber
 function leader.make_leadergrabber(map, args)
-    local args = args or { keys = {}, count = 0, digits = 0 }
+    local args = args or { keys = {}, mods = {}, count = 0, digits = 0 }
     local collect
+    local timer = timer({timeout=leader.timeout})
+    timer:connect_signal("timeout",
+                         function() awful.keygrabber.stop(collect) end)
+    timer:start()
     collect = awful.keygrabber.run(function(mod, key, event)
-            if event == "release"
-                or key:find('Shift')
+            if key:find('Shift')
                 or key:find('Control')
                 or key:find('Alt')
                 or key:find('Super')
             then
+                args.mods[key] = not args.mods[key]
                 return
+            end
+            if event == "release" then
+                return true
             end
             logger.print("fine", "Got key: " .. key)
             if map[key] then
                 table.insert(args.keys, key) 
                 logger.print("fine", "Found callback")
-                map[key](args)
+                map[key].f(args)
+                if map[key].sticky then
+                    timer:again()
+                    return true
+                end
             elseif tonumber(key) then
                 args.count = args.count*10+tonumber(key)
                 args.digits = args.digits+1
@@ -57,18 +69,16 @@ end
 
 --add function for key combination
 --Note: Overwrites functions for prefixes
---an alternative would be to wait for a timeout and then use the old function
-function leader.add_key(word, f)
+function leader.add_key(word, f, sticky)
     local word_len = string.len(word)
     for i=1,word_len do
         local prefix = string.sub(word,1,i)
         local init_prefix = string.sub(word,1,i-1)
         local last_prefix = string.sub(word,i,i)
-        local new_continuation = i == word_len and f
-            or function(args) leader.make_leadergrabber(access_map(prefix), args) end
+        local new_continuation = i == word_len and {f=f, sticky=sticky}
+            or {f=function(args) leader.make_leadergrabber(access_map(prefix), args) end}
         if not access_map(init_prefix) then
-            local map = {}
-            map[last_prefix] = new_continuation
+            local map = {[last_prefix] = new_continuation}
             access_map(init_prefix, map)
         else
             access_map(init_prefix)[last_prefix] = new_continuation
@@ -77,11 +87,19 @@ function leader.add_key(word, f)
 end
 
 
---a function to start grabbing at the root of our map
-function leader.root_leader()
-    leader.make_leadergrabber(access_map(""))
+--a function to get a function to start grabbing keys, leave blank to get root
+function leader.get_leader(word)
+    local word = word or ""
+    return function() leader.make_leadergrabber(access_map(word)) end
 end
 
+function leader.set_timeout(timeout)
+    leader.timeout = timeout
+end
+
+function leader.disable_timeout()
+    leader.timeout = -1
+end
 
 --simple test function
 function leader.setup_test_grabber()
